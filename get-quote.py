c7f9ccf0ff603e64b3941e60a12b3328fc0e1c52
Ishwari <nileshgarge2@gmail.com>
2020-11-09 18:33:52 +0530

    EXTRA

--- get-quote.py
+++ get-quote.py
@@ -7,8 +7,12 @@ f = open("quotes.txt")
 quotes = f.readlines()
 f.close()
 last = 13
-rnd = random.randint(0, last)
-print(quotes[rnd])
+rnd = random.randint(0, last) 
+if rnd <= 11:
+  rnd2 = rnd + 2
+else:
+  rnd2 = rnd - 2
+print(quotes[rnd],quotes[rnd2])
 
 if __name__== "__main__":
   primary()
